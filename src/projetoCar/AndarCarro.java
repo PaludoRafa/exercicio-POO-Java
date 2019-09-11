9e5f67c6f756b1707900365d4d1e32f18e296690
Rafael Paludo <rafaelrppaludo@gmail.com>
2019-09-11 17:21:16 -0300

    Tentativa de criar os métodos

--- src/projetoCar/AndarCarro.java
+++ src/projetoCar/AndarCarro.java
@@ -1,10 +1,37 @@
 package projetoCar;
 
+import java.util.Scanner;
+
 public class AndarCarro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		Scanner scan = new Scanner(System.in);
+		Carro carro = new Carro();
+		/*
+		String modelo;
+		String marca;
+		int qtdMaxCombustivel;
+		int qtdAtualCombustivel;
+		boolean status;
+		int consumoMedio;
+		*/
+		
+		do {
+			System.out.println("Escolha a marca do carro: ");
+			carro.marca = scan.next();
+			System.out.println("Escolha o modelo do carro: ");
+			carro.modelo = scan.next();
+		} while (carro.marca == "" && carro.modelo == "");
+		
+		carro.qtdMaxCombustivel = 50;
+		carro.qtdAtualCombustivel = 50;
+		carro.status = false;
+		carro.consumoMedio = 10;
+		
+		carro.andar(30);
+		
+		
+	};
 }
+
+
--- src/projetoCar/Carro.java
+++ src/projetoCar/Carro.java
@@ -1,5 +1,38 @@
 package projetoCar;
 
 public class Carro {
-
+	String modelo;
+	String marca;
+	int qtdMaxCombustivel;
+	int qtdAtualCombustivel;
+	boolean status;
+	int consumoMedio;
+	
+	public void ligar() {
+		System.out.println("O carro ligou");
+		this.status = true;
+	}
+	
+	public void desligar() {
+		System.out.println("O carro desligou");
+		this.status = false;
+	}
+	
+	public void andar(int km) {
+		System.out.println("O carro andou " + km + " quilômetros");
+		System.out.println("E gastou " + km/this.consumoMedio + " litros");
+	}
+	
+	private void consumirCombustivel(int qtd) {
+		
+	}
+	
+	public void abastecer(int qtd) {
+		//code
+	}
+	
+	private boolean verificarCombustivel(int qtd) {
+		//code
+		return false;
+	}
 }
