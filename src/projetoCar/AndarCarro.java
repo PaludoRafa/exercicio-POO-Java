ef215a19e465f891174deadf0637133a42e94e4f
Rafael Paludo <rafaelrppaludo@gmail.com>
2019-09-11 16:54:15 -0300

    Criado classe principal e main

---
+++ src/projetoCar/AndarCarro.java
@@ -0,0 +1,10 @@
+package projetoCar;
+
+public class AndarCarro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/projetoCar/Carro.java
@@ -0,0 +1,5 @@
+package projetoCar;
+
+public class Carro {
+
+}
